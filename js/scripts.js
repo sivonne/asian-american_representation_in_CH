edfa23e5271eec78fd05916cd464f99a392a48ec
“SivonneWard” <sivonne@live.unc.edu>
2018-12-02 01:13:57 -0500

    added a dropdown menu, need to fix the function but it does show up

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
     console.log('scripts loaded');
+//dropdown JS
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function dropsDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+dropsDown('#myDropdown');
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 //news key
 var mykey = config.MY_KEY;
