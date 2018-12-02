925fac7ab9511e8752f5cb366f0828b23de627e2
“SivonneWard” <sivonne@live.unc.edu>
2018-12-01 22:28:16 -0500

    setting up News API, made first news page for Immigration section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
 $(document).ready(function(){
-  console.log('scripts loaded');
+    console.log('scripts loaded');
 
-});
+//news key
+var mykey = config.MY_KEY;
+
+
+
+
+            $.scrollify({
+            section : "section",
+            sectionName : "section-name",
+            interstitialSection : "",
+            easing: "easeOutExpo",
+            scrollSpeed: 1100,
+            offset : 0,
+            scrollbars: true,
+            standardScrollElements: "",
+            setHeights: true,
+            overflowScroll: true,
+            updateHash: true,
+            touchScroll:true,
+            before:function() {},
+            after:function() {},
+            afterResize:function() {},
+            afterRender:function() {}
+          });
+          $.scrollify.move("#second-section");
+          $.scrollify.move("#third-section");
+          $.scrollify.move("#fourth-section");
+          $.scrollify.move("#fifth-section");
+
+  });
