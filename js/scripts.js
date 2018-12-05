77ffcd0bd411c6203e1a88ef3327bd2dfc4f71b6
“SivonneWard” <sivonne@live.unc.edu>
2018-12-05 01:03:18 -0500

    adding articles and setting up links, need to fix dropdown, update Data USA API, finish adding other APIs and hyperlinking sources on articles

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,68 @@
 $(document).ready(function(){
     console.log('scripts loaded');
+    //DATA USA API AJAX Call
+
+    var url =
+    'http://api.datausa.io/attrs/search/?q=chapel+hill+asian';
+    var data = [];
+    var html = '';
+    var i = '';
+    var articles = [];
+      $.ajax({
+        type:'GET',
+        url:url,
+        dataType:'json',
+        async:true,
+        data:data,
+        success:function(data){
+          console.log(data);
+          articles = data.articles;
+
+          articles.forEach(function(article){
+          console.log(article.title);
+          html += '<div class="latest-news flex">';
+            html +='<img class="thumbnail" src="'+ article.urlToImage + '">';
+            html += '<div class="text">';
+            //must have url to actual article for copyright
+            html += '<a href="' + article.url + '" target="_blank">';
+            html += '<h2 class="headline">' + article.title + '</h2>';
+            html += '<h4 class="byline">by '+ article.author + ', <em>' + article.source.name + '</em></h4>';
+            html += '</a></div';
+          html += '</div>';
+
+});
+      $('#census-section').html(html);
+}
+    });
+
+
+
+
+
+
+
 //dropdown JS
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function dropsDown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-dropsDown('#myDropdown');
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// function dropsDown() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+// dropsDown('#myDropdown');
+// // Close the dropdown menu if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+//
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }
 
 //news key
 var mykey = config.MY_KEY;
@@ -60,33 +101,19 @@ for(i=0; i < urlArray.length; i++){
   $('#news-section').html(html);
   }
   });
-} //close for loop
+}
 
 
 
 
 
-          //   $.scrollify({
-          //   section : "section",
-          //   sectionName : "section-name",
-          //   interstitialSection : "",
-          //   easing: "easeOutExpo",
-          //   scrollSpeed: 1100,
-          //   offset : 0,
-          //   scrollbars: true,
-          //   standardScrollElements: "",
-          //   setHeights: true,
-          //   overflowScroll: true,
-          //   updateHash: true,
-          //   touchScroll:true,
-          //   before:function() {},
-          //   after:function() {},
-          //   afterResize:function() {},
-          //   afterRender:function() {}
-          // });
-          // $.scrollify.move("#second-section");
-          // $.scrollify.move("#third-section");
-          // $.scrollify.move("#fourth-section");
-          // $.scrollify.move("#fifth-section");
+
+
+
+
+
+
+
+
 
   });
