32f393b8289a4afaa6d3679fd6614630ec28f618
“SivonneWard” <sivonne@live.unc.edu>
2018-12-05 11:31:32 -0500

    made some comments, tried to make nav responsive

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     console.log('scripts loaded');
 
+//Google Charts for Data Viz, Sourced from DATA USA
     google.charts.load("current", {packages:["corechart"]});
           google.charts.setOnLoadCallback(drawChart);
           function drawChart() {
@@ -27,7 +28,7 @@ $(document).ready(function(){
             chart.draw(data, options);
           }
 
-          //Google Books API
+          //Google Books API, shows two books about Asian-American studies, then some CSS to make the book flip when hovered over
           google.books.load();
             function alertNotFound() {
                     alert("could not embed the book!");
@@ -40,108 +41,12 @@ $(document).ready(function(){
                 }
           google.books.setOnLoadCallback(initialize);
 
-    //DATA USA API AJAX Call
-
-  //   var url =
-  //   'https://api.datausa.io/api/?sort=desc&force=acs.yg_race&show=geo&sumlevel=all&year=all&geo=16000US3711800';
-  // //   var data = [];
-  //   var html = '';
-  //   var years = [];
-  //     $.ajax({
-  //       type:'GET',
-  //       url:url,
-  //       dataType:'json',
-  //       async:true,
-  //       data:data,
-  //       success:function(data){
-  //         console.log(data);
-  //         //charts js
-  //         var ctx = $("#myChart");
-  //         var myChart = new Chart(ctx,{
-  //         type: 'pie',
-  //         data:{
-  //           datasets: [{
-  //           data: [10, 20, 30]
-  //   }],
-  //         },
-  //         options: options
-  //     });
-  //   }
-  // // });
-  // var dataPoints = [];
-  // var chart = new CanvasJS.Chart("chartContainer",{
-  //     title:{
-  //         text:"Rendering Chart with dataPoints from External JSON"
-  //     },
-  //     data: [{
-  //         type: "line",
-  //         dataPoints : dataPoints,
-  //     }]
-  // });
-  // $.getJSON("/api/records/1.0/search/?dataset=census-diversity&facet=st_abbrev&facet=totpop10&facet=divindx_cy&facet=white_nh&facet=black_nh&facet=amerind_nh&facet=asian_nh&facet=pacific_nh&facet=othrace_nh&facet=hisppop_cy", function(data) {
-  //     $.each(data, function(key, value){
-  //         dataPoints.push({x: value[0], y: parseInt(value[1])});
-  //     });
-  //     chart.render();
-  // });
-
-
-
-
-
-
-
-
-
-//           var ctx = $("#myChart");
-//           var myChart = new Chart(ctx, {
-//           type: 'bar',
-//           dataType:'json',
-//           async:true,
-//           data: {
-//               labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//               datasets: [{
-//                   label: '# of Votes',
-//                   data: [],
-//                   backgroundColor: [
-//                       'rgba(255, 99, 132, 0.2)',
-//                       'rgba(54, 162, 235, 0.2)',
-//                       'rgba(255, 206, 86, 0.2)',
-//                       'rgba(75, 192, 192, 0.2)',
-//                       'rgba(153, 102, 255, 0.2)',
-//                       'rgba(255, 159, 64, 0.2)'
-//                   ],
-//                   borderColor: [
-//                       'rgba(255,99,132,1)',
-//                       'rgba(54, 162, 235, 1)',
-//                       'rgba(255, 206, 86, 1)',
-//                       'rgba(75, 192, 192, 1)',
-//                       'rgba(153, 102, 255, 1)',
-//                       'rgba(255, 159, 64, 1)'
-//                   ],
-//                   borderWidth: 1
-//               }]
-//           },
-//           options: {
-//               scales: {
-//                   yAxes: [{
-//                       ticks: {
-//                           beginAtZero:true
-//                       }
-//                   }]
-//               }
-//           }
-//       });
-// }
-// });
-
-
+  //DATA USA API AJAX Call; for some reason had difficulty getting it to work with a chart or graph, but was able to get it to print to the console so I left this code below to show.
     var url =
-    'http://api.datausa.io/attrs/search/?q=chapel+hill+asian';
-    var data = [];
+    'https://api.datausa.io/api/?sort=desc&force=acs.yg_race&show=geo&sumlevel=all&year=all&geo=16000US3711800';
+  //   var data = [];
     var html = '';
-    var i = '';
-    var articles = [];
+    var years = [];
       $.ajax({
         type:'GET',
         url:url,
@@ -150,45 +55,30 @@ $(document).ready(function(){
         data:data,
         success:function(data){
           console.log(data);
-          articles = data.articles;
+        }
+      });
 
-          articles.forEach(function(article){
-          console.log(article.title);
-          html += '<div class="latest-news flex">';
-            html +='<img class="thumbnail" src="'+ article.urlToImage + '">';
-            html += '<div class="text">';
-            //must have url to actual article for copyright
-            html += '<a href="' + article.url + '" target="_blank">';
-            html += '<h2 class="headline">' + article.title + '</h2>';
-            html += '<h4 class="byline">by '+ article.author + ', <em>' + article.source.name + '</em></h4>';
-            html += '</a></div';
-          html += '</div>';
-
-    });
-      $('#census-section').html(html);
-    }
-    });
-
-
-
-//news key
+//NEWS API to retrieve recent news about Asian-American representation in the news (there seems to be more relevant news closer to the bottom for some reason)
+//news API key
 var mykey = config.MY_KEY;
 var url =
-'https://newsapi.org/v2/everything?q=chapel+hill+representation&from=2018-11-25&sortBy=publishedAt&apiKey='+ mykey;
+'https://newsapi.org/v2/everything?q=asian+american+representation&from=2018-11-25&sortBy=publishedAt&apiKey='+ mykey;
 var data = [];
 var html = '';
 var articles = [];
 var i = '';
+//AJAX call
   $.ajax({
     type:'GET',
     url:url,
     dataType:'json',
     async:true,
     data:data,
+    //prints name of articles in console for debugging
     success:function(data){
       console.log(data.articles);
       articles = data.articles;
-
+//for loop for every article, returns a thumbnail pic, text, headline, byline and author
       articles.forEach(function(article){
       console.log(article.title);
       html += '<div class="latest-news flex">';
@@ -202,46 +92,16 @@ var i = '';
       html += '</div>';
 
   });
+  //places html in the news section at the bottom of the first page
   $('.news-section').html(html);
   }
   });
 
+//to close hamburger menu with cool JS function
+  function myFunction(x) {
+      x.classList.toggle("change");
+  }
 
-
-  //news key
-  var mykey = config.MY_KEY;
-  var url =
-  'https://newsapi.org/v2/everything?q=chapel+hill+representation&from=2018-11-25&sortBy=publishedAt&apiKey='+ mykey;
-  var data = [];
-  var html = '';
-  var articles = [];
-  var i = '';
-    $.ajax({
-      type:'GET',
-      url:url,
-      dataType:'json',
-      async:true,
-      data:data,
-      success:function(data){
-        console.log(data.articles);
-        articles = data.articles;
-
-        articles.forEach(function(article){
-        console.log(article.title);
-        html += '<div class="latest-news flex">';
-          html +='<img class="thumbnail" src="'+ article.urlToImage + '">';
-          html += '<div class="text">';
-          //must have url to actual article for copyright
-          html += '<a href="' + article.url + '" target="_blank">';
-          html += '<h2 class="headline">' + article.title + '</h2>';
-          html += '<h4 class="byline">by '+ article.author + ', <em>' + article.source.name + '</em></h4>';
-          html += '</a></div';
-        html += '</div>';
-
-    });
-    $('.news-section').html(html);
-    }
-    });
 
 
   });
